6f925143901689e860a8302ec2e3b731edbebc6f
01Eddie <jesus.11.97@hotmail.com>
2022-03-12 23:57:54 -0500

    [ADD] files for my_widget

--- src/Components/clock.js
+++ src/Components/clock.js
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 
 const Clock = () => {
   const [ time, setTime ] = useState(new Date());
